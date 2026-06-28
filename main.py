a66f7d793cc48d2030da2b48f68b4bf3890d8741
Vuwori <veeymv@gmail.com>
2026-06-28 21:59:23 +0200

    Completed binary search implementation, fixed mistakes.

--- main.py
+++ main.py
@@ -39,48 +39,40 @@ elif menu_choice == 2:
     print("Binary Search selected! ")
 
     length = int(input(" How many numbers are in your array? "))
-    for x in range(length):
-        
-        binary_search = int(input(" Enter number " + str( x + 1 ) + " :"))
 
+    for x in range(length):
+        binary_search = int(input(" Enter number " + str( x + 1 ) + " :"))
         list_numbers.append(binary_search)
     print(f"Your sequence is {list_numbers}.")
 
-    middle_index = int(len(list_numbers)/2)
     start_index = 0
     end_index = int(len(list_numbers)-1)
 
-    #check for middle index
-    #print(middle_index)
-
-    #check for value of middle index
-    #print(list_numbers[middle_index])
 
     target_bin = int(input("What is your target value? "))
 
-    #looking for target value starting from middle
-    if list_numbers[middle_index] == target_bin:
-        print("You have found the target in the middle!")
+
 
     #looking for if target is greater than middle index
     while start_index <= end_index:
-        new_middle_index = int((start_index+end_index)/2)
-
-        #checks if smaller or greater
-        if target_bin > list_numbers[new_middle_index]:
-            start_index = new_middle_index + 1
-        else:
-            end_index = new_middle_index - 1
         
-        #loops through
+        new_middle_index = int((start_index+end_index)//2)
+
+            #looking for target value starting from middle
         if list_numbers[new_middle_index] == target_bin:
-            print(f"The index is at {new_middle_index}!")
+            print(f"The target was found at index {new_middle_index}!")
+            found = True
             break
-        elif target_bin > new_middle_index:
+
+        #loops through
+        elif target_bin > list_numbers[new_middle_index]:
             start_index = new_middle_index + 1
 
         else: 
             end_index = new_middle_index - 1
+    
+    if not found:
+        print(f"Sorry your target: {target_bin} was not found.")
 
 
 
